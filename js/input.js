b5120de6c11db631594b960730ae24514179b795
bounceball <49814314+bounceball@users.noreply.github.com>
2019-04-22 02:06:26 +0200

    Add files via upload

---
+++ js/input.js
@@ -0,0 +1,219 @@
+var clicks = {leftdown:".",leftup:".",rightdown:".",rightup:".",leftmove:".",rightmove:".",scroll:".", move:false};
+var scrollTimer = 0;
+var moveTimer = 0;
+
+canvas.onmousedown = function(e){
+    if(e.button == 0){
+        clicks["leftdown"] = {x:e.clientX, y:e.clientY};
+        clicks["leftheld"] = true;
+
+        if(clicks["leftdown"].x < standardRadiusBalls){clicks["leftdown"].x = standardRadiusBalls;}
+        if(clicks["leftdown"].x > canvas.width - standardRadiusBalls){clicks["leftdown"].x = canvas.width - standardRadiusBalls;}
+        if(clicks["leftdown"].y < standardRadiusBalls){clicks["leftdown"].y = standardRadiusBalls;}
+        if(clicks["leftdown"].y > canvas.height - standardRadiusBalls){clicks["leftdown"].y = canvas.height - standardRadiusBalls;}
+
+        for (var ball in balls) {
+            if (Math.hypot(balls[ball].x - clicks["leftdown"].x, balls[ball].y - clicks["leftdown"].y) < balls[ball].radius){
+                clicks["move"] = ball;
+                clicks["moved"] = {x:e.clientX, y:e.clientY};
+                balls[ball].x = clicks["leftdown"].x;
+                balls[ball].y = clicks["leftdown"].y;
+                balls[ball].dx = 0;
+                balls[ball].dy = 0;
+                clicks["leftdown"] = {x:".", y:"."};
+            }
+        }
+    }
+    if(e.button == 2){
+        clicks["rightdown"] = {x:e.clientX, y:e.clientY};
+        clicks["rightheld"] = true;
+    }
+    if(e.button == 1){
+        standardRadiusBalls = 30;
+
+        clicks["scroll"] = {x:e.clientX, y:e.clientY};
+        if(scrollTimer != 0){clearTimeout(scrollTimer);}
+        scrollTimer = window.setTimeout("scrollStop()", 500);
+    }
+};
+
+canvas.onmouseup = function(e){
+    if(e.button == 0){
+        clicks["leftup"] = {x:e.clientX, y:e.clientY};
+        clicks["leftheld"] = false;
+
+        balls[balls.length] = {
+            radius:standardRadiusBalls,
+            mass:standardRadiusBalls**3,
+            dx:-(clicks["leftdown"].x-clicks["leftup"].x)/30, 
+            dy:-(clicks["leftdown"].y-clicks["leftup"].y)/30,
+            x:clicks["leftdown"].x,
+            y:clicks["leftdown"].y,
+            color:eval(standardColorBalls),
+        };
+
+        clicks["leftdown"] = {x:".", y:"."};
+        clicks["leftmove"] = {x:".", y:"."};
+        clicks["move"] = false;
+        clearTimeout(moveTimer);
+    }
+    if(e.button == 2){
+        clicks["rightup"] = {x:e.clientX, y:e.clientY};
+        clicks["rightheld"] = false;
+
+        walls[walls.length] = {
+            x1:clicks["rightdown"].x, 
+            y1:clicks["rightdown"].y,
+            x2:clicks["rightup"].x,
+            y2:clicks["rightup"].y
+        };
+
+        clicks["rightdown"] = {x:".", y:"."};
+        clicks["rightmove"] = {x:".", y:"."};
+    }
+};
+
+canvas.onmousemove = function(e){
+    if(clicks["leftheld"]){
+        clicks["leftmove"] = {x:e.clientX, y:e.clientY};
+    }
+
+    if(clicks["rightheld"]){
+        clicks["rightmove"] = {x:e.clientX, y:e.clientY};
+    }
+
+    if(clicks["move"]){
+        if(moveTimer != 0){clearTimeout(moveTimer);}
+        moveTimer = window.setTimeout("moveStop("+clicks["move"]+")", 1);
+        balls[clicks["move"]].x = e.clientX;
+        balls[clicks["move"]].y = e.clientY;
+        balls[clicks["move"]].dx = e.clientX - clicks["moved"].x;
+        balls[clicks["move"]].dy = e.clientY - clicks["moved"].y;
+
+        clicks["moved"] = {x:e.clientX, y:e.clientY};
+    }
+};
+
+canvas.onwheel = function(e){
+    clicks["scroll"] = {x:e.clientX, y:e.clientY};
+    if(scrollTimer != 0){clearTimeout(scrollTimer);}
+    scrollTimer = window.setTimeout("scrollStop()", 250);
+
+    if(e.deltaY < 0){
+        if(standardRadiusBalls < 99){standardRadiusBalls += 2;}
+    }
+    if(e.deltaY > 0){
+        if(standardRadiusBalls > 11){standardRadiusBalls -= 2;}
+    }
+};
+
+function moveStop(ball){
+    balls[ball].dx = 0;
+    balls[ball].dy = 0;
+}
+
+function scrollStop(){
+    clicks["scroll"] = {x:".", y:"."};
+}
+
+var imageCount = 0;
+function previewFile(){
+    var file = document.querySelector('toggle[type=file]').files[0];
+    var reader = new FileReader();
+
+    if(file){
+        reader.readAsDataURL(file);
+    }
+    else{
+        standardColorBalls = "randomColor()";
+    }
+
+    reader.onloadend = function () {
+        imageCount++;
+        document.getElementById("images").innerHTML += "<img src='" + reader.result + "' id='img" + imageCount + "' style='display:none;'/>";
+        standardColorBalls = "image()";
+    }
+}
+
+document.onkeydown = checkKeyDown;
+
+function checkKeyDown(e) {
+
+    e = e || window.event;
+    
+    if (e.keyCode == '37'){ //left arrow
+        if(paused){
+            balls = JSON.parse(JSON.stringify(frameHistory[currentFrame-1]));
+            currentFrame--;
+            if(trail == false){ctx.clearRect(0, 0, canvas.width, canvas.height);}
+            drawobjects();
+        }
+    }
+    if (e.keyCode == '39'){ //right arrow
+        if(paused){
+            if(currentFrame == frameHistory.length || currentFrame == frameHistory.length-1){
+                requestAnimationFrame(draw);
+            }
+            else{
+                balls = JSON.parse(JSON.stringify(frameHistory[currentFrame+1]));
+                currentFrame++;
+                if(trail == false){ctx.clearRect(0, 0, canvas.width, canvas.height);}
+                drawobjects();
+            }
+        }
+    }
+
+    if (e.keyCode == '72'){ //h
+        if(document.getElementById("overlay").style.display == "none"){document.getElementById("overlay").style.display = "block";}
+        else{document.getElementById("overlay").style.display = "none";}
+    }
+    if (e.keyCode == '78'){ //n
+        var radius = Math.floor(Math.random() * (100 -9)) + 10;
+        balls[balls.length] = {
+            radius:radius,
+            mass:radius**3,
+            dx:Math.floor(Math.random() * (25)) + 1,
+            dy:Math.floor(Math.random() * (25)) + 1,
+            x:Math.floor(Math.random() * canvas.width),
+            y:Math.floor(Math.random() * canvas.width),
+            color:eval(standardColorBalls),
+        };
+    }
+    if (e.keyCode == '82'){ //r
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        balls = [];
+        walls = [];
+    }
+    if (e.keyCode == '67'){ //c
+        toggle("collision");
+    }
+    if (e.keyCode == '70'){ //f
+        toggle("friction");
+    }
+    if (e.keyCode == '71'){ //g
+        toggle("gravity");
+    }
+    if (e.keyCode == '80'){ //p
+        toggle("paused");
+    }
+    if (e.keyCode == '84'){ //t
+        toggle("trail");
+    }
+}
+
+function toggle(variable){
+    window[variable] = !window[variable];
+    document.getElementById(variable + "CB").checked = window[variable];
+
+    if(variable == "paused"){
+        if(!paused){requestAnimationFrame(draw);}
+    }
+}
+
+var basegravityScale = 0.5;
+var basefrictionScale = 0.005;
+var basespeed = 1;
+
+function change(variable){
+    window[variable] = window["base" + variable] * Number(document.getElementById(variable).value)/100;
+}
--- js/script.js
+++ js/script.js
@@ -8,8 +8,6 @@ document.addEventListener('contextmenu', event => event.preventDefault());
 var balls = [];
 var walls = [];
 
-var actions = [];
-
 var friction = true;
 var gravity = false;
 var collision = true;
@@ -19,12 +17,17 @@ var standardRadiusBalls = 30;
 var standardColorBalls = "randomColor()";
 
 var gravityScale = 0.5;
-var airFrictionScale = 0.995;
-var bounceFrictionScale = 1; //needs rotation to work
+var frictionScale = 0.005;
 var speed = 1;
+
+var frameHistory = [];
+var currentFrame = 0;
 
 function draw() {
     if(trail == false){ctx.clearRect(0, 0, canvas.width, canvas.height);}
+
+    frameHistory[currentFrame] = JSON.parse(JSON.stringify(balls));
+    currentFrame++;
 
     if(gravity){applyGravity();}
     if(friction){applyFriction();}
@@ -43,8 +46,8 @@ function applyGravity() {
 
 function applyFriction() {
     for(var ball in balls){
-        balls[ball].dx *= airFrictionScale**speed;
-        balls[ball].dy *= airFrictionScale**speed;
+        balls[ball].dx *= (1-frictionScale)**speed;
+        balls[ball].dy *= (1-frictionScale)**speed;
     }
 }
 
@@ -62,7 +65,7 @@ function applyCollision() {
         for(var ball1 in balls){
             for (var ball2 in balls) {
                 if(ball1 < ball2){
-                    if(Math.hypot(balls[ball2].x - balls[ball1].x, balls[ball2].y - balls[ball1].y) < balls[ball1].radius + balls[ball2].radius){
+                    if(Math.hypot(balls[ball2].x - balls[ball1].x, balls[ball2].y - balls[ball1].y) <= balls[ball1].radius + balls[ball2].radius){
                         var theta1 = Math.atan2(balls[ball1].dy, balls[ball1].dx);
                         var theta2 = Math.atan2(balls[ball2].dy, balls[ball2].dx);
                         var phi = Math.atan2(balls[ball2].y - balls[ball1].y, balls[ball2].x - balls[ball1].x);
@@ -71,10 +74,10 @@ function applyCollision() {
                         var v1 = Math.sqrt(balls[ball1].dx**2 + balls[ball1].dy**2);
                         var v2 = Math.sqrt(balls[ball2].dx**2 + balls[ball2].dy**2);
 
-                        balls[ball1].dx = ((v1 * Math.cos(theta1 - phi) * (m1-m2) + 2*m2*v2*Math.cos(theta2 - phi)) / (m1+m2) * Math.cos(phi) + v1*Math.sin(theta1-phi) * Math.cos(phi+Math.PI/2)) * bounceFrictionScale;
-                        balls[ball1].dy = ((v1 * Math.cos(theta1 - phi) * (m1-m2) + 2*m2*v2*Math.cos(theta2 - phi)) / (m1+m2) * Math.sin(phi) + v1*Math.sin(theta1-phi) * Math.sin(phi+Math.PI/2)) * bounceFrictionScale;
-                        balls[ball2].dx = ((v2 * Math.cos(theta2 - phi) * (m2-m1) + 2*m1*v1*Math.cos(theta1 - phi)) / (m1+m2) * Math.cos(phi) + v2*Math.sin(theta2-phi) * Math.cos(phi+Math.PI/2)) * bounceFrictionScale;
-                        balls[ball2].dy = ((v2 * Math.cos(theta2 - phi) * (m2-m1) + 2*m1*v1*Math.cos(theta1 - phi)) / (m1+m2) * Math.sin(phi) + v2*Math.sin(theta2-phi) * Math.sin(phi+Math.PI/2)) * bounceFrictionScale;
+                        balls[ball1].dx = (v1 * Math.cos(theta1 - phi) * (m1-m2) + 2*m2*v2*Math.cos(theta2 - phi)) / (m1+m2) * Math.cos(phi) + v1*Math.sin(theta1-phi) * Math.cos(phi+Math.PI/2);
+                        balls[ball1].dy = (v1 * Math.cos(theta1 - phi) * (m1-m2) + 2*m2*v2*Math.cos(theta2 - phi)) / (m1+m2) * Math.sin(phi) + v1*Math.sin(theta1-phi) * Math.sin(phi+Math.PI/2);
+                        balls[ball2].dx = (v2 * Math.cos(theta2 - phi) * (m2-m1) + 2*m1*v1*Math.cos(theta1 - phi)) / (m1+m2) * Math.cos(phi) + v2*Math.sin(theta2-phi) * Math.cos(phi+Math.PI/2);
+                        balls[ball2].dy = (v2 * Math.cos(theta2 - phi) * (m2-m1) + 2*m1*v1*Math.cos(theta1 - phi)) / (m1+m2) * Math.sin(phi) + v2*Math.sin(theta2-phi) * Math.sin(phi+Math.PI/2);
 
                         var overlap = balls[ball1].radius + balls[ball2].radius - Math.hypot(balls[ball1].x - balls[ball2].x,balls[ball1].y - balls[ball2].y);
                         balls[ball1].x -= overlap * Math.cos(phi)/((balls[ball1].mass/balls[ball2].mass) + 1);
@@ -121,8 +124,8 @@ function applyCollision() {
                     var wallAngle = ((((-(Math.atan2(y - (ball.y + balldy), x - (ball.x + balldx))))+Math.PI*2)%(Math.PI*2))+Math.PI/2)%(Math.PI*2);
                     var newAngle = 2*wallAngle-ballAngle;
                     var ballspeed = Math.sqrt(ball.dx**2 + ball.dy**2);
-                    balls[ball1].dx = Math.cos(newAngle) * ballspeed * bounceFrictionScale;
-                    balls[ball1].dy = -Math.sin(newAngle) * ballspeed * bounceFrictionScale;
+                    balls[ball1].dx = Math.cos(newAngle) * ballspeed;
+                    balls[ball1].dy = -Math.sin(newAngle) * ballspeed;
                 }
 
                 if(Math.hypot(ball.x - x, ball.y - y) < ball.radius){
@@ -136,10 +139,10 @@ function applyCollision() {
     }
 
     for(var ball in balls){
-        if(balls[ball].x < balls[ball].radius){balls[ball].x = balls[ball].radius; balls[ball].dx *= -bounceFrictionScale; balls[ball].dy *= bounceFrictionScale;}
-        if(balls[ball].x > canvas.width - balls[ball].radius){balls[ball].x = canvas.width - balls[ball].radius; balls[ball].dx *= -bounceFrictionScale; balls[ball].dy *= bounceFrictionScale;}
-        if(balls[ball].y < balls[ball].radius){balls[ball].y = balls[ball].radius; balls[ball].dy *= -bounceFrictionScale; balls[ball].dx *= bounceFrictionScale;}
-        if(balls[ball].y > canvas.height - balls[ball].radius){balls[ball].y = canvas.height - balls[ball].radius; balls[ball].dy *= -bounceFrictionScale; balls[ball].dx *= bounceFrictionScale;}
+        if(balls[ball].x < balls[ball].radius){balls[ball].x = balls[ball].radius; balls[ball].dx *= -1;}
+        if(balls[ball].x > canvas.width - balls[ball].radius){balls[ball].x = canvas.width - balls[ball].radius; balls[ball].dx *= -1;}
+        if(balls[ball].y < balls[ball].radius){balls[ball].y = balls[ball].radius; balls[ball].dy *= -1;}
+        if(balls[ball].y > canvas.height - balls[ball].radius){balls[ball].y = canvas.height - balls[ball].radius; balls[ball].dy *= -1;}
     }
 }    
 
@@ -217,211 +220,6 @@ function drawobjects() {
         ctx.lineWidth = 5;
         ctx.strokeStyle = "Black";
         ctx.stroke();
-    }
-}
-
-var clicks = {leftdown:".",leftup:".",rightdown:".",rightup:".",leftmove:".",rightmove:".",scroll:".", move:false};
-var scrollTimer = 0;
-var moveTimer = 0;
-
-document.onkeydown = checkKeyDown;
-
-function checkKeyDown(e) {
-
-    e = e || window.event;
-    
-    if (e.keyCode == '8'){ //backspace
-        if(document.getElementById("settingsTxt").style.display !== "block"){
-            if(actions[actions.length-1] == "ball"){balls.pop(); actions.pop();}
-            else if(actions[actions.length-1] == "wall"){walls.pop(); actions.pop();}
-        }
-    }
-    if (e.keyCode == '39'){ //right arrow
-        if(paused){requestAnimationFrame(draw);}
-    }
-    if (e.keyCode == '72'){ //h
-        if(document.getElementById("overlay").style.display == "none"){document.getElementById("overlay").style.display = "block";}
-        else{document.getElementById("overlay").style.display = "none";}
-    }
-    if (e.keyCode == '78'){ //n
-        var radius = Math.floor(Math.random() * (100 -9)) + 10;
-        balls[balls.length] = {
-            radius:radius,
-            mass:radius**3,
-            dx:Math.floor(Math.random() * (25)) + 1,
-            dy:Math.floor(Math.random() * (25)) + 1,
-            x:Math.floor(Math.random() * canvas.width),
-            y:Math.floor(Math.random() * canvas.width),
-            color: eval(standardColorBalls),
-        };
-        actions.push("ball");
-    }
-    if (e.keyCode == '82'){ //r
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        balls = [];
-        walls = [];
-        
-    }
-    if (e.keyCode == '67'){ //c
-        collision = !collision;
-        if(collision){document.getElementById("collision").innerHTML = "On";}
-        else{document.getElementById("collision").innerHTML = "Off";}
-    }
-    if (e.keyCode == '70'){ //f
-        friction = !friction;
-        if(friction){document.getElementById("friction").innerHTML = "On";}
-        else{document.getElementById("friction").innerHTML = "Off";}
-    }
-    if (e.keyCode == '71'){ //g
-        gravity = !gravity;
-        if(gravity){document.getElementById("gravity").innerHTML = "On";}
-        else{document.getElementById("gravity").innerHTML = "Off";}
-    }
-    if (e.keyCode == '80'){ //p
-        paused = !paused;
-        requestAnimationFrame(draw);
-        if(paused){document.getElementById("pause").innerHTML = "Paused";}
-        else{document.getElementById("pause").innerHTML = "Not Paused";}
-    }
-    if (e.keyCode == '84'){ //t
-        trail = !trail;
-        if(trail){document.getElementById("trail").innerHTML = "On";}
-        else{document.getElementById("trail").innerHTML = "Off";}
-    }
-}
-
-canvas.onmousedown = function(e){
-    if(e.button == 0){
-        clicks["leftdown"] = {x:e.clientX, y:e.clientY};
-        clicks["leftheld"] = true;
-
-        if(clicks["leftdown"].x < standardRadiusBalls){clicks["leftdown"].x = standardRadiusBalls;}
-        if(clicks["leftdown"].x > canvas.width - standardRadiusBalls){clicks["leftdown"].x = canvas.width - standardRadiusBalls;}
-        if(clicks["leftdown"].y < standardRadiusBalls){clicks["leftdown"].y = standardRadiusBalls;}
-        if(clicks["leftdown"].y > canvas.height - standardRadiusBalls){clicks["leftdown"].y = canvas.height - standardRadiusBalls;}
-
-        for (var ball in balls) {
-            if (Math.hypot(balls[ball].x - clicks["leftdown"].x, balls[ball].y - clicks["leftdown"].y) < balls[ball].radius){
-                clicks["move"] = ball;
-                clicks["moved"] = {x:e.clientX, y:e.clientY};
-                balls[ball].x = clicks["leftdown"].x;
-                balls[ball].y = clicks["leftdown"].y;
-                balls[ball].dx = 0;
-                balls[ball].dy = 0;
-                clicks["leftdown"] = {x:".", y:"."};
-            }
-        }
-    }
-    if(e.button == 2){
-        clicks["rightdown"] = {x:e.clientX, y:e.clientY};
-        clicks["rightheld"] = true;
-    }
-    if(e.button == 1){
-        standardRadiusBalls = 30;
-
-        clicks["scroll"] = {x:e.clientX, y:e.clientY};
-        if(scrollTimer != 0){clearTimeout(scrollTimer);}
-        scrollTimer = window.setTimeout("scrollStop()", 500);
-    }
-};
-
-canvas.onmouseup = function(e){
-    if(e.button == 0){
-        clicks["leftup"] = {x:e.clientX, y:e.clientY};
-        clicks["leftheld"] = false;
-
-        balls[balls.length] = {
-            radius:standardRadiusBalls,
-            mass:standardRadiusBalls**3,
-            dx:-(clicks["leftdown"].x-clicks["leftup"].x)/30, 
-            dy:-(clicks["leftdown"].y-clicks["leftup"].y)/30,
-            x:clicks["leftdown"].x,
-            y:clicks["leftdown"].y,
-            color: eval(standardColorBalls),
-        };
-        actions.push("ball");
-
-        clicks["leftdown"] = {x:".", y:"."};
-        clicks["leftmove"] = {x:".", y:"."};
-        clicks["move"] = false;
-        clearTimeout(moveTimer);
-    }
-    if(e.button == 2){
-        clicks["rightup"] = {x:e.clientX, y:e.clientY};
-        clicks["rightheld"] = false;
-
-        walls[walls.length] = {
-            x1:clicks["rightdown"].x, 
-            y1:clicks["rightdown"].y,
-            x2:clicks["rightup"].x,
-            y2:clicks["rightup"].y
-        };
-        actions.push("wall");
-
-        clicks["rightdown"] = {x:".", y:"."};
-        clicks["rightmove"] = {x:".", y:"."};
-    }
-};
-
-canvas.onmousemove = function(e){
-    if(clicks["leftheld"]){
-        clicks["leftmove"] = {x:e.clientX, y:e.clientY};
-    }
-
-    if(clicks["rightheld"]){
-        clicks["rightmove"] = {x:e.clientX, y:e.clientY};
-    }
-
-    if(clicks["move"]){
-        if(moveTimer != 0){clearTimeout(moveTimer);}
-        moveTimer = window.setTimeout("moveStop("+clicks["move"]+")", 1);
-        balls[clicks["move"]].x = e.clientX;
-        balls[clicks["move"]].y = e.clientY;
-        balls[clicks["move"]].dx = e.clientX - clicks["moved"].x;
-        balls[clicks["move"]].dy = e.clientY - clicks["moved"].y;
-
-        clicks["moved"] = {x:e.clientX, y:e.clientY};
-    }
-};
-
-canvas.onwheel = function(e){
-    clicks["scroll"] = {x:e.clientX, y:e.clientY};
-    if(scrollTimer != 0){clearTimeout(scrollTimer);}
-    scrollTimer = window.setTimeout("scrollStop()", 250);
-
-    if(e.deltaY < 0){
-        if(standardRadiusBalls < 99){standardRadiusBalls += 2;}
-    }
-    if(e.deltaY > 0){
-        if(standardRadiusBalls > 11){standardRadiusBalls -= 2;}
-    }
-};
-
-function moveStop(ball){
-    balls[ball].dx = 0;
-    balls[ball].dy = 0;
-}
-
-function scrollStop(){
-    clicks["scroll"] = {x:".", y:"."};
-}
-
-var imageCount = 0;
-function previewFile(){
-    var file = document.querySelector('input[type=file]').files[0];
-    var reader = new FileReader();
-
-    if(file){
-        reader.readAsDataURL(file);
-    }
-    else{
-        standardColorBalls = "randomColor()";
-    }
-
-    reader.onloadend = function () {
-        imageCount++;
-        document.getElementById("images").innerHTML += "<img src='" + reader.result + "' id='img" + imageCount + "' style='display:none;'/>";
-        standardColorBalls = "image()";
     }
 }
 
